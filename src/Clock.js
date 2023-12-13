882548013aa2416cb0d0a77e59e4f1cb51472d92
Ryan Joesting <nsd-rjoesting@nucor.com>
2023-12-13 09:46:21 -0600

    Updated colors

--- src/Clock.js
+++ src/Clock.js
@@ -10,8 +10,9 @@ function Clock() {
             <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="200px" height="200px">
                 <defs>
                     <linearGradient id="GradientColor">
-                        <stop offset="0%" stopColor="#e91e63" />
-                        <stop offset="100%" stopColor="#673ab7" />
+                        <stop offset="0%" stopColor="#d64bf2" />
+                        <stop offset="30%" stopColor="#a607f2" />
+                        <stop offset="100%" stopColor="#470a8e" />
                     </linearGradient>
                 </defs>
                 <circle cx="100" cy="100" r="85" strokeLinecap="round" />
