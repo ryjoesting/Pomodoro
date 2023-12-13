89e4c39bed4198ade2ec9d0a3fa0a7acd5ae8c1b
Ryan Joesting <nsd-rjoesting@nucor.com>
2023-12-13 09:23:30 -0600

    Animation, fixed alignment of svg

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
-import './styles/App.css';
 import Clock from './Clock'
 
 function App() {
   return (
-    <div className='clock-container'>
-      <Clock />
-    </div>
+    <Clock />
   );
 }
 
--- src/Clock.js
+++ src/Clock.js
@@ -2,8 +2,20 @@ import './styles/Clock.css'
 
 function Clock() {
     return (
-        <div className='outer'>
-            <div className="inner"></div>
+        <div className="clock-container">
+            <div className='outer'>
+                <div className="inner"><h1 className='time'>## %</h1></div>
+            </div>
+
+            <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="200px" height="200px">
+                <defs>
+                    <linearGradient id="GradientColor">
+                        <stop offset="0%" stopColor="#e91e63" />
+                        <stop offset="100%" stopColor="#673ab7" />
+                    </linearGradient>
+                </defs>
+                <circle cx="100" cy="100" r="85" strokeLinecap="round" />
+            </svg>
         </div>
     );
 }
